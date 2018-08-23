1c4124523d4b844c222a80a4de09e560334c1e96
Byungchan Gil <gilbc890@gmail.com>
2018-08-23 12:41:42 -0400

    Aug. 23rd 18, React Practice(movie_app)

---
+++ src/Movie.js
@@ -0,0 +1,50 @@
+import React from 'react';
+import propTypes from 'prop-types';
+import './Movie.css';
+
+function Movie({title, poster, genres, synopsis}){
+  return(
+    <div className="Movie">
+      <div className="Movie__colums">
+        <MoviePoster poster={poster} alt={title}/>
+      </div>
+      <div className="Movie__colums">
+        <h1>{title}</h1>
+        <div className="Movie__Genres">
+          {genres.map((genre, index) => <MovieGenre genre={genre} key={index}/>)}
+        </div>
+        <p className="Movie__synopsis">
+          {synopsis}
+        </p>
+      </div>
+    </div>
+  )
+}
+
+function MoviePoster({poster, alt}){
+  return(
+    <img src={poster} alt={alt} title={alt} className="Movie__Poster"/>
+  )
+}
+function MovieGenre({genre}){
+  return(
+    <span className="Movie__Genre">{genre} </span>
+  )
+}
+
+Movie.propTypes = {
+  title:propTypes.string.isRequired,
+  poster:propTypes.string.isRequired,
+  genres:propTypes.array.isRequired,
+  synopsis:propTypes.string.isRequired
+}
+
+MoviePoster.propTypes = {
+  poster: propTypes.string.isRequired,
+  alt:propTypes.string.isRequired
+}
+MovieGenre.propTypes = {
+  genre: propTypes.string.isRequired
+}
+
+export default Movie
